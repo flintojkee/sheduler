9b0db6913867aa82923949a4bc287233c0f89830
DenysVasylenko <32224385+flintojkee@users.noreply.github.com>
2018-07-08 04:50:16 +0300

    background change

--- assets/main.js
+++ assets/main.js
@@ -2,8 +2,8 @@ var day = true;
 function switchShift() {
     var dayShift = document.getElementsByClassName("day-shift");
     var nightShift = document.getElementsByClassName("night-shift");
-
    if (!day) {
+       document.body.style.backgroundImage = "url('assets/day.svg')";
         for( i = 0; i < dayShift.length; i++){
             dayShift[i].classList.remove("hide");
         }
@@ -12,7 +12,7 @@ function switchShift() {
         }
         day = true;
     } else {
-
+       document.body.style.backgroundImage = "url('assets/night.png')";
         for( i = 0; i < nightShift.length; i++){
             nightShift[i].classList.remove("hide");
         }
